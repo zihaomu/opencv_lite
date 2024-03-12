303aaf7dd54a20b3231ed79b6f1427e51bf62477
zihaomu <zihaomu@outlook.com>
2024-03-12 14:22:15 +0800

    remove unused code

--- modules/dnn/test/test_tflite_backend.cpp
+++ modules/dnn/test/test_tflite_backend.cpp
@@ -126,22 +126,6 @@ TEST_P(Test_TFLite, max_unpooling)
 //    imwrite("img_hair.jpg", img);
 }
 
-//TEST_P(Test_TFLite, EfficientDet_int8) {
-//    Net net = readNet(findDataFile("dnn/tflite/coco_efficientdet_lite0_v1_1.0_quant_2021_09_06.tflite", false));
-//
-//    Mat img = imread(findDataFile("dnn/dog416.png"));
-//    Mat blob = blobFromImage(img, 1.0, Size(320, 320));
-//
-//    net.setInput(blob);
-//    Mat out = net.forward();
-//    Mat_<float> ref({3, 7}, {
-//        0, 7, 0.62890625, 0.6014542579650879, 0.13300055265426636, 0.8977657556533813, 0.292389452457428,
-//        0, 17, 0.56640625, 0.15983937680721283, 0.35905322432518005, 0.5155506730079651, 0.9409466981887817,
-//        0, 1, 0.5, 0.14357104897499084, 0.2240825891494751, 0.7183101177215576, 0.9140362739562988
-//    });
-//    normAssertDetections(ref, out, "", 0.5, 0.05, 0.1);
-//}
-
 TEST_P(Test_TFLite, MobilenetV1_128_int8)
 {
     Net net = readNet(findDataFile("dnn/tflite/mobilenet_v1_0.25_128_quant.tflite", false));
@@ -157,8 +141,6 @@ TEST_P(Test_TFLite, MobilenetV1_128_int8)
     Mat out = net.forward();
     auto outName = net.getOutputName();
     Mat ref = blobFromNPY(findDataFile("dnn/tflite/mobilenet_v1_0.25_128_quant_out_0.npy"));
-    printMatData(ref);
-    printMatData(out);
     normAssert(ref.reshape(1, 1), out.reshape(1, 1), outName[0].c_str(), l1, lInf);
 }
 
